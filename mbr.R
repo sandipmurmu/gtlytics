ffe9b83f8dd5b38d3d5dee2540cdb69a53408ac5
Sandip Murmu <sandip.murmu@silicus.com>
2015-08-05 17:55:10 +0530

    modified

--- mbr.R
+++ mbr.R
@@ -1,8 +1,33 @@
 
 
-mbr = read.csv(file.choose(), header=TRUE)
-ds = mbr[c("gender","marital_status","occupation", "total_visits")]
-summary(ds)
-  
+mbr = read.csv(file.choose(), header=TRUE, na.strings=c("","NA"))
+
+ds=mbr
+#ds = mbr[c("gender","marital_status","occupation", "total_visits")]
+
+df = data.frame(ds)
+
+propmiss = function(df) {
+  m = sapply(df, function(x) {
+    data.frame(
+      nmiss=sum(is.na(x)), 
+      n=length(x), 
+      propmiss=sum(is.na(x))/length(x)
+    )
+  })
+  d = data.frame(t(m))
+  d = sapply(d, unlist)
+  d = as.data.frame(d)
+  d$variable = row.names(d)
+  row.names(d) =NULL
+  d = cbind(d[ncol(d)],d[-ncol(d)])
+  return(d[order(d$propmiss), ])
+
+}
+
+propmiss(df)
+
+
+
 
 
