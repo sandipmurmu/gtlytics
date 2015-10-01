d2688a80d001a268021e905d9cccdec90da9e5ab
Sandip Murmu <sandip.murmu@silicus.com>
2015-10-01 21:05:02 +0530

    updated

--- surv_analysis.R
+++ surv_analysis.R
@@ -1,10 +1,11 @@
 #read customers data 
 customers = read.csv(file.choose(), header=TRUE)
-head(customers)
+#head(customers)
 #create a data frame from the data set
 cdf = as.data.frame(customers)
 
-#survival analysis for various churn levels
+###survival analysis for various churn levels
+
 #install.packages("survival")
 library(survival)
 
@@ -13,10 +14,18 @@ set.seed(1000)
 s1= cdf[sample(nrow(cdf),1000, replace=FALSE, prob = NULL),]
 
 #derive response variable
-resp = Surv(s1$First_Purchase, s1$Last_Purchase, s1$Churn_30)
+resp = Surv(s1$First_Purchase, s1$Last_Purchase, s1$Churn_450)
 #Fit a survival curve
-fit = survfit(resp ~ 1, data=s1)
+#fit = survfit(resp ~ 1, data=s1)
 #plot the survival curve
-surgraph = plot(fit, lty = 1, mark.time = FALSE, ylim=c(.75,1), xlab = "Days since subscribed", ylab="Percent Surviving")
+#surgraph = plot(fit, lty = 1, mark.time = FALSE, ylim=c(.75,1), xlab = "Days since subscribed", ylab="Percent Surviving")
+
+#build a model
+model = coxph(resp ~ s1$NumOfVisits + s1$VisitInterval,data = s1)
+summary(model)
+#Plot the survival model
+plot(survfit(model), xscale=365.25,xlab = "Days Active", ylab = "Proportion Survived", main="Hazard curve")
+
+
 
             
