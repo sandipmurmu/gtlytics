805392c47746beb05868af9c19b9e0fe67736f84
Mursan <sandip.murmu@gmail.com>
2015-10-04 03:09:49 +0530

    update

--- surv_analysis.R
+++ surv_analysis.R
@@ -17,28 +17,27 @@ s1= cdf[sample(nrow(cdf),1000, replace=FALSE, prob = NULL),]
 response = Surv(time=s1$First_Purchase, time2 = s1$Last_Purchase, event = s1$Churn_450)
 
 #Kaplain-Meier estimator with no covariates
-km.fit = survfit(response ~ 1,type="kaplan-meier", conf.type="log",data=s1)
+km.surv = survfit(response ~ 1,type="kaplan-meier",data=s1)
+summary(km.surv)
+plot(km.surv, lty = 1, mark.time = FALSE, xscale=365.25, ylim=c(.75,1), xlab = "Days Active", ylab="Percent Surviving")
 
-plot(km.fit, lty = 1, mark.time = FALSE, ylim=c(.75,1), xlab = "Days Active", ylab="Percent Surviving")
+#Kaplain-Meier estimator by gender
+km.surv.gender = survfit(response ~ s1$Sex,type="kaplan-meier",data=s1)
+summary(km.surv.gender)
+plot(km.surv.gender, lty = 1, mark.time = FALSE, xscale = 365.25, ylim=c(.75,1), xlab = "Days Active", ylab="Percent Surviving")
 
 
-summary(km.fit)
-#summary(km.fit, times = c(60,90,120,130))
+#Cox Regression using coxph function for coefficients and hazard rates
+cox.response = coxph(formula = response ~ s1$NumOfVisits + s1$VisitInterval,data = s1)
+summary(cox.response)
+#plot(survfit(cox.model), xscale=365.25,xlab = "Days Active", ylab = "Proportion Survived", main="Hazard curve")
+#predict hazard ratio
+predRes <- predict(cox.response, type="risk")
+head(predRes)
 
-#install.packages("rms")
-library(rms)
-survplot(fit = km.fit)
 
-#build a model
-model = coxph(response ~ s1$NumOfVisits + s1$VisitInterval,data = s1)
-summary(model)
-#Plot the survival model
-plot(survfit(model), xscale=365.25,xlab = "Days Active", ylab = "Proportion Survived", main="Hazard curve")
 
-fit = survfit(response ~ s1$Sex, data=s1)
-plot(fit, lty = 1:2, mark.time = FALSE, ylim=c(.75,1), xlab = "Days Active", ylab="Percent Surviving") 
-legend(20, .8, c("M", "F"), lty = 1.2, bty = "n",ncol = 2)
 
-survdiff(resp ~ s1$Sex, data = s1)
+
 
             
