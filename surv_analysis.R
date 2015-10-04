28687b0af87b0bd60cd1ade401b03fbdc6b9e78c
Mursan <sandip.murmu@gmail.com>
2015-10-05 01:04:23 +0530

    updated

--- surv_analysis.R
+++ surv_analysis.R
@@ -12,23 +12,25 @@ library(survival)
 #1000 samples for test 1, churn 
 set.seed(1000)
 s1= cdf[sample(nrow(cdf),1000, replace=FALSE, prob = NULL),]
+attach(s1)
 
 #derive response variable, referred as survival object
-response = Surv(time=s1$First_Purchase, time2 = s1$Last_Purchase, event = s1$Churn_450)
+response = Surv(ActivePeriod, event = Churn_450)
 
 #Kaplain-Meier estimator with no covariates
 km.surv = survfit(response ~ 1,type="kaplan-meier",data=s1)
 summary(km.surv)
-plot(km.surv, lty = 1, mark.time = FALSE, xscale=365.25, ylim=c(.75,1), xlab = "Days Active", ylab="Percent Surviving")
+#plot(km.surv)
+plot(km.surv, mark.time = FALSE, ylim=c(.75,1),xlab = "Days Active", ylab="Percent Surviving")
 
 #Kaplain-Meier estimator by gender
-km.surv.gender = survfit(response ~ s1$Sex,type="kaplan-meier",data=s1)
+km.surv.gender = survfit(response ~ Sex,type="kaplan-meier",data=s1)
 summary(km.surv.gender)
-plot(km.surv.gender, lty = 1, mark.time = FALSE, xscale = 365.25, ylim=c(.75,1), xlab = "Days Active", ylab="Percent Surviving")
+plot(km.surv.gender, mark.time = FALSE, ylim=c(.75,1), xlab = "Days Active", ylab="Percent Surviving")
 
 
 #Cox Regression using coxph function for coefficients and hazard rates
-cox.response = coxph(formula = response ~ s1$NumOfVisits + s1$VisitInterval,data = s1)
+cox.response = coxph(formula = response ~ NumOfVisits + VisitInterval,data = s1)
 summary(cox.response)
 #plot(survfit(cox.model), xscale=365.25,xlab = "Days Active", ylab = "Proportion Survived", main="Hazard curve")
 #predict hazard ratio
@@ -36,8 +38,9 @@ predRes <- predict(cox.response, type="risk")
 head(predRes)
 
 
+#Paramteric analysis
+exp = survreg(response ~ NumOfVisits + VisitInterval, data = s1, dist = "exponential")
+summary(exp)
 
-
-
-
-            
+weibull = survreg(response ~ NumOfVisits + VisitInterval, data=s1, dist = "weibull")
+summary(weibull)
