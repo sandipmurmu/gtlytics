186c5ec7c292cdc68f2e05ec77ba7560bcbe8904
Sandip Murmu <sandip.murmu@silicus.com>
2015-10-06 21:13:33 +0530

    updated

--- surv_analysis.R
+++ surv_analysis.R
@@ -17,13 +17,14 @@ attach(s1)
 
 
 #derive response variable, referred as survival object
-response = Surv(time=ceiling(ActivePeriod/30), event = Churn_90)
+response = Surv(time=ceiling(ActivePeriod/30), event = Churn_365)
 
 #Kaplain-Meier estimator with no covariates
 km.surv = survfit(response ~ 1,type="kaplan-meier",data=s1)
 summary(km.surv)
-plot(km.surv, mark.time = FALSE, ylim=c(.7,1), xlab = "Months", ylab="Percent Surviving")
+plot(km.surv, mark.time = FALSE, xlab = "Months", ylab="Proportion Surviving")
 grid()
+
 
 #Kaplain-Meier estimator by gender
 km.surv.gender = survfit(response ~ Sex,type="kaplan-meier",data=s1)
@@ -34,13 +35,15 @@ grid()
 
 
 #Cox Regression using coxph function for coefficients and hazard rates
-cox.response = coxph(formula = response ~ NumOfVisits + VisitInterval,data = s1)
-summary(cox.response)
-plot(survfit(cox.model), xscale=365.25,xlab = "Days Active", ylab = "Proportion Survived", main="Hazard curve")
+cox.model = coxph(formula = response ~ NumOfVisits + VisitInterval,data = s1)
+cox.model
+#survival analysis for cox model
+summary(survfit(cox.model))
+plot(survfit(cox.model),xlab = "Month", ylab = "Proportion Survived", main="Hazard curve",mark.time = FALSE)
 
 #predict hazard ratio
-cox.zph(cox.response)
-plot(km.surv)
+cox.zph(cox.model)
+
 grid()
 
 #predRes <- predict(cox.response, type="risk")
