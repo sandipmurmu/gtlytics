63020cefc40023a3dab1635e6e22fb4b7f5c8fbc
Mursan <sandip.murmu@gmail.com>
2015-10-07 04:17:17 +0530

    updated

--- surv_analysis.R
+++ surv_analysis.R
@@ -1,41 +1,44 @@
 #read customers data 
-customers = read.csv(file.choose(), header=TRUE)
+ds = read.csv(file.choose(), header=TRUE)
 #head(customers)
 #create a data frame from the data set
-cdf = as.data.frame(customers)
+cdf = as.data.frame(ds)
 
+
+detach(mini)
+#1000 samples randomly taken 
+mini= cdf[sample(nrow(cdf),1000, replace=FALSE, prob = NULL),]
+attach(mini)
 
 #install.packages("survival")
 library(survival)
 
-
-set.seed(1000)
-detach(s1)
-#1000 samples randomly taken 
-s1= cdf[sample(nrow(cdf),1000, replace=FALSE, prob = NULL),]
-attach(s1)
-
-
 #derive response variable, referred as survival object
-response = Surv(time=ceiling(ActivePeriod/30), event = Churn_365)
+response = Surv(ActivePeriod, Churn_60)
 
 #Kaplain-Meier estimator with no covariates
-km.surv = survfit(response ~ 1,type="kaplan-meier",data=s1)
+km.surv = survfit(response ~ 1,type="kaplan-meier",data=mini)
 summary(km.surv)
-plot(km.surv, mark.time = FALSE, xlab = "Months", ylab="Proportion Surviving")
+plot(km.surv, mark.time = FALSE, xlab = "Days", ylab="Proportion Surviving")
 grid()
 
 
 #Kaplain-Meier estimator by gender
-km.surv.gender = survfit(response ~ Sex,type="kaplan-meier",data=s1)
+km.surv.gender = survfit(response ~ Sex,type="kaplan-meier",data=mini)
 summary(km.surv.gender)
-plot(km.surv.gender, mark.time = FALSE, ylim=c(.75,1), xlab = "Days Active", ylab="Percent Surviving")
+plot(km.surv.gender, mark.time = FALSE,  xlab = "Days Active", ylab="Percent Surviving")
 grid()
 
 
 
+#Nelson-aalen
+km.surv.alen = survfit(coxph(Surv(cdf$days,cdf$churn)~1), type="aalen")
+summary(km.surv.alen)
+
+res = Surv(cdf$days,cdf$churn, type = "aalen")
+
 #Cox Regression using coxph function for coefficients and hazard rates
-cox.model = coxph(formula = response ~ NumOfVisits + VisitInterval,data = s1)
+cox.model = coxph(formula = response ~ NumOfVisits + VisitInterval,data = mini)
 cox.model
 #survival analysis for cox model
 summary(survfit(cox.model))
