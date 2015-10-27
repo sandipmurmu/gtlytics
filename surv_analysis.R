56aec3d1986a5b250f622b0c13d1d861e5c35464
Sandip Murmu <sandip.murmu@silicus.com>
2015-10-27 21:28:36 +0530

    UPDATED

--- surv_analysis.R
+++ surv_analysis.R
@@ -2,6 +2,29 @@
 ds = read.csv(file.choose(), header=TRUE)
 cdf = as.data.frame(ds)
 
+#create a R, F variables 
+cdf$RSeg = findInterval(cdf$RecentVisit, quantile(cdf$RecentVisit, c(0.0, 0.25, 0.50, 0.75, 1.0), na.rm = TRUE))
+cdf$FSeg = findInterval(cdf$NumOfVisits, quantile(cdf$NumOfVisits, c(0.0, 0.25, 0.50, 0.75, 1.0), na.rm = TRUE))
+cdf$MSeg = findInterval(cdf$AvgPurchase, quantile(cdf$AvgPurchase, c(0.0, 0.25, 0.50, 0.75, 1.0), na.rm = TRUE))
+#cdf$F = cut(cdf$NumOfVisits, 5, labels = F)
+#cdf$R = cut(cdf$RecentVisit, 5, labels =F)
+library(xlsx)
+write.xlsx(cdf, file = 'D:/statistics/churn_rf.xlsx', sheetName = "Churn", col.names = TRUE)
+
+cdf.rfm = cdf[c(1,17:19)]
+row.has.na <- apply(cdf.rfm, 1, function(x){any(is.na(x))})
+sum(row.has.na)
+final.rfm = cdf.rfm[!row.has.na,]
+km = kmeans(final.rfm, centers = 5)
+aggregate(final.rfm, by = list(km$cluster), FUN = mean)
+final.rfm = data.frame(final.rfm, km$cluster)
+
+#subset
+#library(sqldf)
+#cdf.sub = sqldf("select ClientID, Sex, First_Purchase, Last_Purchase, NumOfVisits, VisitInterval, TotalPurchase, AvgPurchase, RecentVisit from cdf where Churn_90=1")
+km = kmeans(cdf.sub, centers = 3)
+
+
 #plot the distribution of the data set
 days = cdf$ActivePeriod
 d = density(days)
